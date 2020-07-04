a7152a251d53862024a9408ad2e804cae75209ad
Gustavo Margutti <gustavo.margutti@hotmail.com>
2020-07-04 14:49:30 -0300

    Basic Error Middleware

---
+++ src/Middlewares/ErrorMiddleware.ts
@@ -0,0 +1,15 @@
+import {
+  Request, Response, NextFunction, ErrorRequestHandler,
+} from 'express';
+
+export default function errorMiddleware(
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  if (err.name === 'CUSTOM_ERROR') {
+    return res.status(400).json({ message: err.message });
+  }
+  return res.status(500).json({ message: 'Não foi possível concluir a requisição!' });
+}
